7481eeebda319a1325a0d6ee2bffcbb25e2f07ec
Soma <73409799+smakela13@users.noreply.github.com>
2021-07-25 01:09:26 -0400

    Updated create_app function in init file - Created app/init file and added imports - Created home.py and the routes

--- app/__init__.py
+++ app/__init__.py
@@ -11,5 +11,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'hello world'
+    
+    # register routes
+    app.register_blueprint(home)
 
     return app
